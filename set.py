188637f3ac0841fe257a820795b3c625beb96d97
s-shubham-22 <20ce123@charusat.edu.in>
2022-01-27 21:35:29 +0530

    .

---
+++ set.py
@@ -0,0 +1,9 @@
+# a. Write a Python program to add member(s) in a set and clear a set
+
+# b. Write a Python program to remove an item from a set if it is present in the set.
+
+# c. Write a Python program to create an intersection, Union, difference of sets.
+
+# d. Write a Python program to find maximum and the minimum value in a set.
+
+# e. Write a Python program to find the most common elements and their counts from list, tuple, dictionary.
