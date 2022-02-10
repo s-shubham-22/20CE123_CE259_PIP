723fa0513614c325f90fe2108cc9a59b18e15a89
Shubham Sareliya <sareliashubham9999@gmail.com>
2022-02-10 11:10:12 +0530

    itertools updated

--- itertools/itertools.py
+++ itertools/itertools.py
@@ -5,7 +5,9 @@
 # https://www.geeksforgeeks.org/python-itertools/
 
 
+from bz2 import compress
 import itertools
+import operator
 
 # count(start, step)
 print("count(start, step)")
@@ -14,9 +16,10 @@ for i in itertools.count(5, 5):
         break
     else:
         print(i, end =" ")
+print()
 
 # cycle(p)
-print()
+print("\ncycle(p)")
 count = 0
 for i in itertools.cycle('AB'):
     if count > 7:
@@ -24,8 +27,38 @@ for i in itertools.cycle('AB'):
     else:
         print(i, end = " ")
         count += 1
+print()
 
 # repeat(val, num)
+print("\nrepeat(val, num)")
 print (list(itertools.repeat(25, 4)))
+print()
 
+# accumulate(list, func)
+print("accumulate(list, func)")
+li = [1, 2, 3, 4, 5]
+print(list(itertools.accumulate(li, lambda x, y: x * y)))
+print()
+
+# chain(list1, list2)
+print("chain(list1, list2)")
+print(list(itertools.chain('ABC', 'DEF')))
+print()
+
+# chain.from_iterable(list_of_lists)
+# list_of_lists = ['ABC', 'DEF']
+print("chain.from_iterable(list_of_lists)")
+list_of_lists = ['ABC', 'DEF']
+print(list(itertools.chain.from_iterable(list_of_lists)))
+print()
+
+# compress(data, selectors)
+print("compress(data, selectors)")
+data = [1, 2, 3, 4, 5]
+selectors = [True, False, True, True, False]
+print(list(itertools.compress(data, selectors)))
+print()
+
+# dropwhile(predicate, iterable)
+print("dropwhile(predicate, iterable)")
 
