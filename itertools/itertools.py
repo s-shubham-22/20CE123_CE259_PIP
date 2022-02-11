c8839de4290b1b0b7898a4e40e91f6cdcd1f46a7
s-shubham-22 <20ce123@charusat.edu.in>
2022-02-11 09:49:46 +0530

    itertools completed

--- itertools/itertools.py
+++ itertools/itertools.py
@@ -60,5 +60,53 @@ print(list(itertools.compress(data, selectors)))
 print()
 
 # dropwhile(predicate, iterable)
-print("dropwhile(predicate, iterable)")
+print("dropwhile(predicate, iterable)") # using dropwhile() to start displaying after condition is false
+li = [2, 4, 5, 8, 9]
+print (list(itertools.dropwhile(lambda x : x % 2 == 0, li)))
+print()
+
+# filterfalse(predicate, iterable)
+print("filterfalse(predicate, iterable)") # using filterfalse() to print false values
+print(list(itertools.filterfalse(lambda x : x % 2 == 0, li)))
+print()
+
+# groupby(iterable, keyfunc)
+print("groupby(iterable, keyfunc)")
+key_pair = [('a', 1), ('a', 2), ('b', 3), ('c', 4), ('c', 5)]
+for key, group in itertools.groupby(key_pair, lambda x : x[0]):
+    print(f'{key}: {list(group)}')
+print()
+
+# islice(iterable, start, stop, step)
+print("islice(iterable, start, stop, step)")
+print(list(itertools.islice(range(10), 2, 8, 2))) # start, stop, step
+
+# pairwise(iterable)
+print("pairwise(iterable)")
+print(list(itertools.pairwise('ABCDE')))
+print()
+
+# starmap(function, iterable)
+print("starmap(function, iterable)")
+print(list(itertools.starmap(operator.add, [(1, 2), (3, 4), (5, 6)])))
+print()
+
+# takewhile(predicate, iterable)
+print("takewhile(predicate, iterable)")
+print(list(itertools.takewhile(lambda x : x < 5, [1, 2, 3, 4, 5, 6, 7, 8, 9])))
+print()
+
+# tee(iterator, count)
+print("tee(iterator, count)")
+li = [1, 2, 3, 4, 5]
+iti = iter(li)
+it = itertools.tee(iti, 3)
+for i in range(3):
+    print(list(it[i]))
+print()
+
+# zip_longest(p, q, fillvalue=None)
+print("zip_longest(p, q, fillvalue=None)")
+print(list(itertools.zip_longest('ABCD', 'xy', fillvalue='-')))
+print()
 
